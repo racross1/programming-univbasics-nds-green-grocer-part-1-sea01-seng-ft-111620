3098dbc2d28cb5aee28ec9ee8754ce269be82d63
Rachel Cross <racross1@gmail.com>
2020-11-15 15:55:27 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,119 @@
+require 'pry'
+
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
-
+  result = nil
+  collection.each do |item_info|
+    if item_info[:item] == name 
+      result = item_info
+    end
+  end
+p result
+#binding.pry
 end
 
+
+#left off trying to find how to generate countnums 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  receipt = []
+  item_names = []
+  aoh_item_counts = []
+  countnum = 3
+  cart.each do |item_info|
+    individual_item = find_item_by_name_in_collection(item_info[:item], cart)
+    individual_item[:count] = countnum
+    receipt << individual_item
+    #binding.pry
+  # if !receipt[individual_item]
+  #   receipt << individual_item
+end 
+receipt
+binding.pry
+end 
 
+def countnums(cart)
+  cart.count()
+  binding.pry
 end
+end 
+  
+#   countnums_array = []
+#   cart.each do |item_info|
+#     item_names << item_info[:item]
+#     end 
+# item_names.each do |name|
+#   name_count = item_names.count(name)
+#   countnums_array << {name => name_count}
+#   end 
+# binding.pry
+# countnums_array
+# end 
+  
+    
+#def count_item(cart, item)
+  #intended to count instances of a given item in cart and return countnum
+    
+    
+    
+    
+    
+    
+    
+#     item_names << item_info[:item]
+#   end 
+#   item_names.each do |name|
+#     name_count = item_names.count(name)
+#     aoh_item_counts << {name=> {:count => name_count}}
+#   end
+# aoh_item_counts
+# end 
+ 
+ 
+ 
+ #cart [:count = [countname]
+ 
+#   aoh_item_counts.each do |itemcount_hash|
+#     itemcount_hash.each do |key, value|
+#       item_info_hash = find_item_by_name_in_collection(key, cart)
+#       item_info_hash << value
+#     end 
+#   end 
+#   item_info_hash
+#   binding.pry
+# end 
+  
+  #match up item count with each item and then add to a new array
+  
+  
+    
 
+
+  
+  
+#   final_results = cart.each_with_object({}) do |(key, value), final_array|
+#     value.each do |inner_key, names|
+#       names.each do |name|
+#         if !final_array[name]
+#           final_array[name] = {}
+#         end 
+#         if !final_array[name][key]
+#           !final_array[name][key] = []
+#         end 
+#         final_array[name][key].push(inner_key.to_s)
+#         binding.pry
+#       end 
+#     end 
+#   end 
+# end 
+  
+  
+    
+    
+    
+#   binding.pry
+
+
+# end
+
+# The consolidate_cart method should do something similar - it should take in a 'cart', an array of hashes of individual items and return a new array of hashes, but with each item containing a quantity. So, for example, given the following cart:
 
   
